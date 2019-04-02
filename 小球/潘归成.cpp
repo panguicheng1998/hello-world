37b19158d5b74068ab85a9b7de598baa24b02a46
panguicheng1998 <49156337+panguicheng1998@users.noreply.github.com>
2019-04-02 12:46:43 +0800

    hf
    jhg

---
+++ 小球/潘归成.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+void main()
+{
+	int i,j;
+	int x = 5; 
+	int y = 10;
+	char input;
+
+	while (1)
+	{
+		system("cls");   
+		for(i=0;i<x;i++)
+			printf("\n");
+		for (j=0;j<y;j++)
+			printf(" ");
+		printf("*");  
+		printf("\n"); 
+			
+}
